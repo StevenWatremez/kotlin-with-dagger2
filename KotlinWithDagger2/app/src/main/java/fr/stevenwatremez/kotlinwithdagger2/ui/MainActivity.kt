de23433ecf47cadec0ceed90c9744d068a8addff
Steven Watremez <steven.watremez@niji.fr>
2017-05-28 11:55:13 +0200

    init project with kotlin

---
+++ KotlinWithDagger2/app/src/main/java/fr/stevenwatremez/kotlinwithdagger2/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package fr.stevenwatremez.kotlinwithdagger2.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import fr.stevenwatremez.kotlinwithdagger2.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+}
